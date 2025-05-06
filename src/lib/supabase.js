8a880bc6ea0b05321af3fa3d895e76107a23d4dc
Frvncois <hello@frvncois.com>
2025-05-06 10:47:57 -0400

    enf

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,13 +1,12 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://ruwscznlijhccdfawrgb.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ1d3Njem5saWpoY2NkZmF3cmdiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYzNjU4NjgsImV4cCI6MjA2MTk0MTg2OH0.1K9d1YIS1LhkEK1CgGzryW7ebEkh20RYn6J_KOqqIyQ'
-
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: true,
-  }
+  },
 })
